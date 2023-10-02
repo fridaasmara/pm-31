abf2086fddc417211fce668e9141099783396677
fridaasmara <113350657+fridaasmara@users.noreply.github.com>
2023-10-02 12:56:11 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,7 @@
+const login = (namaDepan, namaBelakang, email) => {
+  console.log("Login Screen");
+  console.log("Welcome " + namaDepan + " " + namaBelakang);
+  console.log("Your Email : " + email);
+};
+
+login("frida", "asmara", "fridaasmara.id@gmail.com");
---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+function home(namaDepan, namaBelakang, email) {
+  console.log("Selamat Datang di Home Screen");
+  console.log("Halo " + namaDepan + " " + namaBelakang);
+  console.log("email kamu : " + email);
+}
+
+home("Frida", "Asmara", "fridaasmara.id@gmail.com");
